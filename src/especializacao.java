295215d1fc6fe2591efbb8b9c67446bfb4e12d2f
Samuka <samuelbalbino153@gmail.com>
2025-10-01 21:10:24 -0300

    Só add classe de especializacao

---
+++ src/especializacao.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class especializacao {
+    private ArrayList<String> especs = new ArrayList<>();
+    private String especss;
+    public void adicionarEspecs(String especcs){
+        especs.add(especcs);
+//to mal, hj só vai dar isso msm, amanhã tento fazer mais
+    }
+}
